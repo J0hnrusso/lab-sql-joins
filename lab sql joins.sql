ebc16f88cf90a6c8fbdc77811a6fe06b9b6c48f6
J0hnrusso <joao.russo.figueiredo@gmail.com>
2025-01-21 19:13:17 +0000

    revision

--- lab sql joins.sql
+++ lab sql joins.sql
@@ -37,3 +37,60 @@ INNER JOIN film AS c
     ON a.film_id = c.film_id
 GROUP BY b.name
 ORDER BY average_length DESC;
+
+-- 6
+SELECT 
+    m.title AS Movie_Title, 
+    COUNT(r.inventory_id) AS Rental_Count
+FROM 
+    rental r
+JOIN 
+    inventory i 
+ON 
+    r.inventory_id = i.inventory_id
+JOIN 
+    film m 
+ON 
+    i.film_id = m.film_id
+GROUP BY 
+    m.title
+ORDER BY 
+    Rental_Count DESC
+LIMIT 10;
+-- 7
+SELECT 
+    COUNT(i.inventory_id) AS Number_of_Copies,
+    CASE 
+        WHEN COUNT(r.inventory_id) < COUNT(i.inventory_id) THEN 'Available in Store 1'
+        ELSE 'Not Available in Store 1'
+    END AS Availability
+FROM 
+    inventory i
+JOIN 
+    film m 
+ON 
+    i.film_id = m.film_id
+LEFT JOIN 
+    rental r 
+ON 
+    i.inventory_id = r.inventory_id AND r.return_date IS NULL
+WHERE 
+    m.title = 'Academy Dinosaur'
+    AND i.store_id = 1;
+-- 8
+SELECT 
+    f.title AS Film_Title,
+    CASE 
+        WHEN IFNULL(COUNT(i.inventory_id), 0) > 0 THEN 'Available'
+        ELSE 'NOT Available'
+    END AS Availability_Status
+FROM 
+    film f
+LEFT JOIN 
+    inventory i 
+ON 
+    f.film_id = i.film_id
+GROUP BY 
+    f.title
+ORDER BY 
+    Availability_Status DESC, f.title;
